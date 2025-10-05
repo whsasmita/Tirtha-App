0c41923a666762a9f1faeaa8a24ad438d06c4996
Wahyu Harta Sasmita <118668406+whsasmita@users.noreply.github.com>
2025-10-05 09:05:04 +0800

    update

---
+++ lib/presentation/themes/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Warna Primer
+  static const Color primary = Color(0xFF008080);
+  static const Color secondary = Color(0xFF00C29F);
+  static const Color tertiary = Color(0xFFF5922F);
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFF4A4A4A);
+  static const Color textSecondary = Color(0xFFFFFFFF);
+}
