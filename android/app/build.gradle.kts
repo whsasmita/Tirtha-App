60ac1e81f1bd90f9654df2acb5f24fa16d3b397e
wahyu harta sasmita <wahyusasmitaharta561@gmail.com>
2025-10-26 17:56:14 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,15 +11,15 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
 
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        // jvmTarget = JavaVersion.VERSION_11.toString()
-        jvmTarget = "17"
+        jvmTarget = JavaVersion.VERSION_11.toString()
+        // jvmTarget = "17"
     }
 
     defaultConfig {
@@ -28,7 +28,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 33
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
